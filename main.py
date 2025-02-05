6bf1c534147acf58497be1a7ddda5ea1f1213490
AdaheK <emmanueladahe2003@gmail.com>
2025-02-05 16:33:39 +0100

    modification fizzbuzz

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 
 def main():
-    print('Hello, World!')
+    list = []
+    for i in range(1,101):
+        if i%3==0 and i%5==0:
+            print("FizzBuzz")
+        else:
+            if i%3==0:
+                print("Fizz")
+            elif i%5==0:
+                print("Buzz")
+            else:
+                print(i)
     return 0
 
 if __name__ == '__main__':
