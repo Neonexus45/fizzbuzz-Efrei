f195053e774b3e5ee8497dc26cc8e1c0ee15110e
JojoLePopo <joseph.destatguillot@gmail.com>
2025-02-05 16:17:11 +0100

    gfgdx

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-
 def main():
-    print('Hello, World!')
-    return 0
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
 
 if __name__ == '__main__':
     main()
-
