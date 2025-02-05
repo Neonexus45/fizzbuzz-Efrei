8c4a94583f803ff0efa7527cbc2b38a6dd1d4821
Mohellibi <yamohe2021@gmail.com>
2025-02-05 17:14:36 +0100

    test

--- main.py
+++ main.py
@@ -10,7 +10,31 @@ def main():
     else:
         print(i)
 
+def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
+
+for i in range(1, 101):
+    print(fizzbuzz(i))
+
+# Test function
+def test_fizzbuzz():
+    assert fizzbuzz(1) == "1"
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(30) == "FizzBuzz"
+    assert fizzbuzz(98) == "98"
+    print("All tests passed!")
+
+
 
 if __name__ == '__main__':
     main()
-
+    test_fizzbuzz()
