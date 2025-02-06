028676a2db13f61dc26b5369644e4da6fa0346e2
JojoLePopo <joseph.destatguillot@gmail.com>
2025-02-06 10:01:48 +0100

    yes

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    result = []
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            result.append("FizzBuzz")
+        elif num % 3 == 0:
+            result.append("Fizz")
+        elif num % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(num)
+    return result
---
+++ test.py
@@ -0,0 +1,16 @@
+from fizzbuzz import fizzbuzz
+import unittest
+
+class TestFizzBuzz(unittest.TestCase):
+    
+    def test_fizzbuzz(self):
+        result = fizzbuzz()
+        self.assertEqual(len(result), 100)
+        self.assertEqual(result[14], "FizzBuzz")
+        self.assertEqual(result[2], "Fizz")
+        self.assertEqual(result[4], "Buzz")
+        self.assertEqual(result[0], 1)
+
+if __name__ == '__main__':
+    unittest.main()
+()
