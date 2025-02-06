d9777c39e7f53837fad7662160b3449f1df0d53a
judekabeya <judek9392@gmail.com>
2025-02-06 10:20:38 +0100

    update

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,27 +1,22 @@
-import unittest
-import main
-#test
-class TestFizzBuzz(unittest.TestCase):
+import pytest
+import main  
 
-    def multiple_3(self):
-        self.assertEqual(main.fizzbuzz(3), "Fizz")
-        self.assertEqual(main.fizzbuzz(6), "Fizz")
-        self.assertEqual(main.fizzbuzz(9), "Fizz")
+def multiple_3():
+    assert main.fizzbuzz(3) == "Fizz"
+    assert main.fizzbuzz(6) == "Fizz"
+    assert main.fizzbuzz(9) == "Fizz"
 
-    def multiple_5(self):
-        self.assertEqual(main.fizzbuzz(5), "Buzz")
-        self.assertEqual(main.fizzbuzz(10), "Buzz")
-        self.assertEqual(main.fizzbuzz(20), "Buzz")
+def multiple_5():
+    assert main.fizzbuzz(5) == "Buzz"
+    assert main.fizzbuzz(10) == "Buzz"
+    assert main.fizzbuzz(20) == "Buzz"
 
-    def multiple_3_and_5(self):
-        self.assertEqual(main.fizzbuzz(15), "FizzBuzz")
-        self.assertEqual(main.fizzbuzz(30), "FizzBuzz")
-        self.assertEqual(main.fizzbuzz(45), "FizzBuzz")
+def multiple_3_and_5():
+    assert main.fizzbuzz(15) == "FizzBuzz"
+    assert main.fizzbuzz(30) == "FizzBuzz"
+    assert main.fizzbuzz(45) == "FizzBuzz"
 
-    def not_multiple_3_et_5(self):
-        self.assertEqual(main.fizzbuzz(1), 1)
-        self.assertEqual(main.fizzbuzz(2), 2)
-        self.assertEqual(main.fizzbuzz(4), 4)
-
-if __name__ == "__main__":
-    unittest.main()
+def not_multiple_3_et_5():
+    assert main.fizzbuzz(1) == 1
+    assert main.fizzbuzz(2) == 2
+    assert main.fizzbuzz(4) == 4
