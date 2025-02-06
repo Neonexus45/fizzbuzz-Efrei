4b977019f790f57bcafd7b775a207d28d30aa339
MariusOzanne <ozanne.marius@gmail.com>
2025-02-06 14:43:05 +0100

    ajout du programme fizzbuzz de base

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ def fizzbuzz(num):
         return "buzz"
     else:
         return str(num)
+    
+def main():
+    for i in range(1, 101):
+        if i%3 == 0 and i % 5 == 0:
+            return "fizzbuzz"
+        elif i%3 == 0:
+            return "fizz"
+        elif i % 5 == 0:
+            return "buzz"
+        else:
+            return str(i)
