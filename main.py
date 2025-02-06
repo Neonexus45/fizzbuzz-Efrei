17c064d3dcab08115277673ceb6306731f0bde76
JojoLePopo <joseph.destatguillot@gmail.com>
2025-02-06 09:55:41 +0100

    salut

--- main.py
+++ main.py
@@ -9,5 +9,30 @@ def main():
         else:
             print(num)
 
+def fizzbuzz():
+    result = []
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            result.append("FizzBuzz")
+        elif num % 3 == 0:
+            result.append("Fizz")
+        elif num % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(num)
+    return result
+
+import unittest
+
+class TestFizzBuzz(unittest.TestCase):
+    
+    def test_fizzbuzz(self):
+        result = fizzbuzz()
+        self.assertEqual(len(result), 100)
+        self.assertEqual(result[14], "FizzBuzz")
+        self.assertEqual(result[2], "Fizz")
+        self.assertEqual(result[4], "Buzz")
+        self.assertEqual(result[0], 1)
+
 if __name__ == '__main__':
-    main()
+    unittest.main()
